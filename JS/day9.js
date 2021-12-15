c0c074d188e19de9a745700fb64070a38416e421
Kyle Peck <kyle.peck@west-mec.org>
2021-12-14 18:36:57 -0700

    Adding

--- JS/day9.js
+++ JS/day9.js
@@ -3,21 +3,34 @@ const fs = require("fs");
 
 const answerA = { final: 0 };
 
-console.log(input);
-
 //r is for row,
 //c is for column
 //we check to see if the value exists, if it does then we use it if not we give it a value of -1 to make sure it is less
-for(r of input){
-  for(c of input[r]){
-    const current = input[r][c]
-    const right = input[r+1][c] ? input[r+1][c] : -1
-    const left = input[r-1][c] ? input[r+1][c] : -1
-    const up = input[r][c+1] ? input[r+1][c] : -1
-    const down = input[r][c-1] ? input[r+1][c] : -1
+const maxRows = input.length;
+const maxCols = input[0].length;
+for (r in input) {
+  for (c in input[r]) {
+    const current = input[r][c];
+    let up, down, left, right;
+    if (r == 99) {
+      up = -1;
+    } else if (r == 0) {
+      down = -1;
+    }
 
-    if(current > right && current > left && current > up && current >down){
-      answerA.final += current + 1
+    if (c == 99) {
+      right = -1;
+    } else if (c == 0) {
+      left = -1;
+    }
+    console.log(r, c, input[+r][c], right);
+    if (!up) up = input[+r + 1][c];
+    if (!down) down = input[+r - 1][c];
+    if (!right) right = input[r][+c + 1];
+    if (!left) left = input[r][+c - 1];
+
+    if (current > right && current > left && current > up && current > down) {
+      answerA.final += +current + 1;
     }
   }
 }
@@ -27,4 +40,5 @@ const answerB = { final: 0 };
 fs.writeFileSync("../out/day9.json", JSON.stringify({ answerA, answerB }), {
   encoding: "utf-8",
   flag: "w",
-});``
+});
+``;
