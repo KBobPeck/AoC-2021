f54ed5b259269ee4c24eeeaf0e717971f0ea3cc8
Kyle Peck <Kypeckmail@gmail.com>
2021-12-24 03:39:50 -0700

    merging

--- Python/day1.py
+++ Python/day1.py
@@ -1,24 +1,21 @@
 
-from os import write
-
-
 with open('uploads/day1.js', 'r') as file:
     filestring = file.read()
 
+
 filestring = filestring.split('[  ')[1].split(',\n')[0:-1]
-print(filestring)
+
 
 answerA = 0
 for i in range(len(filestring)-1):
-  if int (filestring[i]) < int(filestring[i+1]):
-    answerA += 1
+    if int(filestring[i]) < int(filestring[i+1]):
+        answerA += 1
 
 print(answerA)
 
 answerB = 0
 for i in range(len(filestring)-3):
-  if int(filestring[i]) < int(filestring[i+3]):
-    answerB = answerB+1
+    if int(filestring[i]) < int(filestring[i+3]):
+        answerB = answerB+1
 
 print(answerB)
-
