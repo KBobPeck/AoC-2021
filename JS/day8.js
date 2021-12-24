ce590b45e91ead8c8c4458980d60a58f4d55147b
Kyle Peck <Kypeckmail@gmail.com>
2021-12-24 03:39:26 -0700

    merging

--- JS/day8.js
+++ JS/day8.js
@@ -18,48 +18,59 @@ right
 
 const answerB = { final: 0 };
 
-// find the 1, 4, 7, 8 numbers and define the ones we know
-// The bars are numbered clockwise starting the top and then the middle in number 7
-// {1: 2: 3: 4: 5: 6: 7:} and then there will be a list of not possibles for that section.
+// we know 1, 4, 7, 8
+/*
+2 = 1
+3 = 7
+4 = 4
+5 = 2 3 5
+6 = 0 6 9 
+7 = 8
 
-// check 1 is there only 1 more place the letter can go
-// check 2 is there only 1 letter missing from that place
+the positions are     11
+                    6    2
+                      77
+                    5    3
+                      44
+  !#1 is the size 2
+  !#7 is the size 3
+  !#4 is the size 4
+  !#8 is the size 7
+  //pos1 = #7 - #1
+  !#6 is the 6 size one that does not have all of #1 included
+  *pos2 = #8 - #6
+  *pos3 = #1 - pos2
+  !#5 is the 5 size one missing pos2
+  *pos5 = #5 + pos2 - #8
+  !#0 is the 6 size that includes pos5
+  !#9 is the last 6 size
+  //pos7 = #8 - #0
+  //pos6 = #4 - pos2 - pos3 - pos7
+  !#2 is the size 5 missing pos3
+  !#3 is the remaining size 5 
+*/
 
-const possible = {
-  1: ["a", "b", "c", "d", "e", "f", "g"],
-  2: ["a", "b", "c", "d", "e", "f", "g"],
-  3: ["a", "b", "c", "d", "e", "f", "g"],
-  4: ["a", "b", "c", "d", "e", "f", "g"],
-  5: ["a", "b", "c", "d", "e", "f", "g"],
-  6: ["a", "b", "c", "d", "e", "f", "g"],
-  7: ["a", "b", "c", "d", "e", "f", "g"],
-};
+left.forEach(each => {
+  const splitLeft = each.split(' ');
 
-input.forEach((each, index) => {
-  const fullString = each.split(" | ").join(" ").split(" ");
-  let thisPossible = { ...possible };
+  //stores the sorted sizes
+  answerB.sorted={}
+  //stores the letters that belong to that corresponding number
+  answerB.numberLetters={}
 
-  for (word of fullString) {
-    if (word.length == 2) {
-      const [letter1, letter2] = word;
-      let tempFilterList = ["a", "b", "c", "d", "e", "f", "g"].filter(
-        (each) => each != letter1 || each != letter2
-      );
-      for (i = 1; i <= 7; i++) {
-        thisPossible[i] = thisPossible[i].filter((each) => each != letter1 || each != letter2)
-      }
-    } else if (word.length == 3) {
-    } else if (word.length == 4) {
-    } else if (word.length == 7) {
-    }
-  }
+  //this sorts the letters into their corresponding sizes
+  splitLeft.forEach(each => {
+    const length = each.length
+    answerB.sorted[length] 
+      ? answerB.sorted[length].push(each)
+      :answerB.sorted[length] = [each] 
+  })
 
-  // console.log(thisPossible);
-});
+answerB.numberLetters['1'] = answerB.sorted
+})
 
-// console.log(input);
 
-fs.writeFileSync("../out/day8.json", JSON.stringify({ answerA }), {
+fs.writeFileSync("../out/day8.json", JSON.stringify({ answerA, answerB }), {
   encoding: "utf-8",
   flag: "w",
 });
